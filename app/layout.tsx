88781cc17a42265536d333bd83b5d9e638e4660b
higoruserevi <higor@userevi.com>
2025-07-14 12:05:47 -0300

    fix(layout_header): alter title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Batatas Recheadas',
+  description: 'Batatas recheadas com sabor, crocância e alegria irresistível.',
 }
 
 export default function RootLayout({
